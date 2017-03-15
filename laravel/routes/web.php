26da011da00a523347940500f5182a4ff07befee
king052188 <me@kpa21.info>
2017-03-15 16:09:37 +0800

    Integrate Maintenance Page

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,13 +11,14 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('layout.maintenance');
+});
 
-Route::get('/', 'TestController@index');
+//Route::get('/', 'TestController@index');
 
 Route::post('/account-kit/process', 'TestController@account_kit_response');
+
 
 Route::get('/login', 'MemberController@member_sign_in_index');
 Route::post('/login/processing', 'MemberController@member_sign_in_validation');
@@ -27,10 +28,8 @@ Route::get('/sign-up', 'MemberController@member_sign_up_index');
 Route::post('/sign-up/processing', 'MemberController@member_sign_up_execute');
 
 Route::get('/dashboard', 'MemberController@dashboard_index');
-
 Route::get('/settings', 'MemberController@settings_index');
 Route::post('/settings/change-password', 'MemberController@settings_change_password');
-
 Route::get('/logout', 'MemberController@member_sign_out_process');
 
 
